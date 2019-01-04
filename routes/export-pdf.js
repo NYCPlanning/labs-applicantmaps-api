3f7eec818233b569b4f934cbd3e649f9b338eed3
Chris Whong <chris@whong.org>
2019-01-04 14:16:56 -0500

    better error handling

--- routes/export-pdf.js
+++ routes/export-pdf.js
@@ -18,18 +18,22 @@ switch (process.env.NODE_ENV) {
 async function generatePdf(id, format = 'Tabloid', landscape = true) {
   const url = `${host}/projects/${id}/edit/map/edit`;
 
-  const browser = await puppeteer.launch({
-    dumpio: true,
-    args: ['--disable-setuid-sandbox', '--no-sandbox'],
-  });
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle0' });
-  const buffer = await page.pdf({
-    format,
-    landscape,
-  });
+  try {
+    const browser = await puppeteer.launch({
+      dumpio: true,
+      args: ['--disable-setuid-sandbox', '--no-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const buffer = await page.pdf({
+      format,
+      landscape,
+    });
 
-  return buffer;
+    return buffer;
+  } catch (e) {
+    throw (Error(e));
+  }
 }
 
 router.get('/:id', async (req, res) => {
@@ -40,9 +44,13 @@ router.get('/:id', async (req, res) => {
   const format = size === 'tabloid' ? 'Tabloid' : 'Letter';
   const landscape = (orientation === 'landscape');
 
-  const body = await generatePdf(encodeURIComponent(id), format, landscape);
-  res.setHeader('Content-type', 'application/pdf');
-  res.send(body);
+  try {
+    const body = await generatePdf(encodeURIComponent(id), format, landscape);
+    res.setHeader('Content-type', 'application/pdf');
+    res.send(body);
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 module.exports = router;
