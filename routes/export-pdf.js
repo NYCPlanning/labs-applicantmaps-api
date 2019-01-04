6d6bb419831a320c34c00fb31a0f82f3ebc4b451
Chris Whong <chris@whong.org>
2019-01-04 11:29:29 -0500

    add no-sandbox flag

--- routes/export-pdf.js
+++ routes/export-pdf.js
@@ -18,7 +18,7 @@ switch (process.env.NODE_ENV) {
 async function generatePdf(id, format = 'Tabloid', landscape = true) {
   const url = `${host}/projects/${id}/edit/map/edit`;
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle0' });
   const buffer = await page.pdf({
